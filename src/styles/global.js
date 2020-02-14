d7e8fbf978cbdb00af68e727f86b8aaa7039dd7e
Dtesch9 <alucard.s8123@gmail.com>
2020-02-14 20:21:33 -0300

    feat(pages): styles

---
+++ src/styles/global.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+import background from '../assets/images/background.svg';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+
+    text-decoration: none;
+  }
+
+  body {
+    background: #191920 url(${background}) no-repeat center top;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  body, input, button {
+    font: 14px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 1020px;
+    margin: 0 auto;
+    padding: 0 20px 50;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
