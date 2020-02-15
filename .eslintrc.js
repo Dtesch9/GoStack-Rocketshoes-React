e79563417a7e62d7caf4ab23731c0e579da27181
Dtesch9 <alucard.s8123@gmail.com>
2020-02-14 21:44:23 -0300

    feat: API call

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,7 @@ module.exports = {
       { extensions: ['.jxs', '.js'] }
     ],
     'import/prefer-default-export': 'off',
+    "react/state-in-constructor": [2, 'never'],
     'jsx-a11y/control-has-associated-label': 'off'
   },
 };
